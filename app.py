790c255c63e62d48312081d9ab3b3fff3e85370f
Gerald Lu <lujerry131@gmail.com>
2024-02-11 04:13:56 -0500

    models updated

--- app.py
+++ app.py
@@ -32,17 +32,20 @@ def home_page():
 def prediction():
     data = request.get_json()
     birth_year = int(data.get('yearOfBirth', 0))
-    debt = int(data.get('debt', 0))
-    for slider_name, slider_value in data.items():
-        print(f"Slider '{slider_name}' value: {slider_value}")
+    averageDaysPastDue = int(data.get('averageDaysPastDue', 0))
+    totalLoanPayments = int(data.get('totalLoanPayments', 0))
+    numberofInstallments = int(data.get('numberofInstallments', 0))
+    percentageofLateInstallment = float(data.get('percentageofLateInstallment', 0))
+    interestRate = float(data.get('interestRate', 0))
     model = Model()
-    print(model.predict(birth_year, debt))
-    return jsonify({'message': 'Slider values received successfully'})
+    return jsonify({'message': "Risk: " + "{:.2f}".format(model.predict(birth_year, averageDaysPastDue,totalLoanPayments,numberofInstallments,percentageofLateInstallment, interestRate)*100) + "%"})
 
-
+@app.route('/insights', methods=['POST'])
+def insights():
+    return render_template('insights.html')
 
 class Model:
-    def __init__(self, dir="saved_models"):
+    def __init__(self, dir="lgb"):
         dir_files = [f"{dir}/{file}" for file in os.listdir(dir)]
         self.models = []
         for file in dir_files:
@@ -51,18 +54,45 @@ class Model:
         self.input = pd.read_parquet(dir + "/example.parquet")
         self.input.loc[:] = np.nan
 
-    def predict(self, birth_year, debt):
-        birth_date = (datetime(birth_year, 1, 1) - datetime(1900, 1, 1)).days
-        for column in self.input.columns:
-            if column.startswith("birthdate") or column.startswith("dateofbirth"):
-                self.input.loc[0, column] = birth_date
-        self.input.loc[0, "totaldebt_9A"] = debt
+    # ==============VARIABLE DESCRIPTIONS===============
+    # birth_year: birth year
+    # avg_dpd: Average DPD (days past due) with tolerance within the past 24 months from the maximum closure date, assuming that the contract is finished. If the contract is ongoing, the calculation is based on the current date.
+    # pay_count: Total number of loan payments made by the client.
+    # num_early_inst: Number of instalments paid more than three days before the due date.
+    # perc_late_inst: Percentage of installments that are paid 1 or more days after the due date.
+    # interest: interest rate
+    # ===============VARIABLE RANGES====================
+    # birth_year: [1960, 2000]
+    # avg_dpd: [0, 10]
+    # pay_count: [0, 60]
+    # num_early_inst: [0, 60]
+    # perc_late_inst: [0, 1]
+    # interest: [0, 1]
+    def predict(self, birth_year=None, avg_dpd=None, pay_count=None,
+                num_early_inst=None, perc_late_inst=None,
+                interest=None):
+        if birth_year is not None:
+            birth_date = (datetime(birth_year, 1, 1) - datetime(1900, 1, 1)).days
+            for column in self.input.columns:
+                if column.startswith("birthdate") or column.startswith("dateofbirth"):
+                    self.input.loc[0, column] = birth_date
+        if avg_dpd is not None:
+            self.input.loc[0, "avgdpdtolclosure24_3658938P"] = avg_dpd
+        if pay_count is not None:
+            self.input.loc[0, "pmtnum_254L"] = pay_count
+        if num_early_inst is not None:
+            self.input.loc[0, "numinstpaidearly3d_3546850L"] = num_early_inst
+        if perc_late_inst is not None:
+            self.input.loc[0, "pctinstlsallpaidlate1d_3546856L"] = perc_late_inst
+        if interest is not None:
+            self.input.loc[0, "eir_270L"] = interest
         out = 0
         for model in self.models:
             out += float(model.predict_proba(self.input)[0, 1])
         out /= len(self.models)
         self.input.loc[:] = np.nan
         return out
+
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -12,7 +12,8 @@ function rangeSlide(value) {
     var sliderValues = {};
     var sliders = document.querySelectorAll('.range');
     var sliderValueSpans = document.querySelectorAll('.slider-value');
-    sliders.forEach(function(slider, index) {
+
+    sliders.forEach(function(slider,index) {
         sliderValues[slider.getAttribute('name')] = slider.value;
         sliderValueSpans[index].innerHTML = slider.value;
     });
@@ -20,6 +21,42 @@ function rangeSlide(value) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/prediction", true);
     xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                document.querySelector('.blob-text').textContent = response.message;
+            } else {
+                console.error('Error occurred while making AJAX request:', xhr.status);
+            }
+        }
+    };
     xhr.send(JSON.stringify(sliderValues));
-    
 }
+$(document).ready(function() {
+    $('#viewInsightsButton').on('click', function(event) {
+        event.preventDefault(); 
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/insights", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    var response = JSON.parse(xhr.responseText);
+                    if (response && response.redirect) {
+                        console.log("Redirecting...");
+                        window.location.href = response.redirect;
+                    } else {
+                        console.error('Error: No redirect URL provided in response');
+                    }
+                } else {
+                    console.error('Error occurred while making AJAX request:', xhr.status);
+                }
+            }
+        };
+        xhr.send();
+    });
+});
+
+
