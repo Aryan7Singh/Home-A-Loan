b7519098e8a6b1b16fd0151e3e5c37aafa07bc89
Gerald Lu <lujerry131@gmail.com>
2024-02-11 07:40:45 -0500

    model integrated fully

--- app.py
+++ app.py
@@ -31,25 +31,50 @@ def home_page():
 @app.route('/prediction', methods=['POST'])
 def prediction():
     data = request.get_json()
-    birth_year = int(data.get('yearOfBirth', 0))
-    averageDaysPastDue = int(data.get('averageDaysPastDue', 0))
-    totalLoanPayments = int(data.get('totalLoanPayments', 0))
-    numberofInstallments = int(data.get('numberofInstallments', 0))
-    percentageofLateInstallment = float(data.get('percentageofLateInstallment', 0))
-    interestRate = float(data.get('interestRate', 0))
-    model = Model()
-    return jsonify({'message': "Risk: " + "{:.2f}".format(model.predict(birth_year, averageDaysPastDue,totalLoanPayments,numberofInstallments,percentageofLateInstallment, interestRate)*100) + "%"})
+    slider_values = data.get('sliderValues', {})
+    birth_year = int(slider_values.get('yearOfBirth', 0))
+    averageDaysPastDue = int(slider_values.get('averageDaysPastDue', 0))
+    totalLoanPayments = int(slider_values.get('totalLoanPayments', 0))
+    numberofInstallments = int(slider_values.get('numberofInstallments', 0))
+    percentageofLateInstallment = float(slider_values.get('percentageofLateInstallment', 0)) / 100
+    interestRate = float(slider_values.get('interestRate', 0)) / 100
+    selected_models = data.get('selectedModels', [])
+    model = Model(selected_models)
+    return jsonify({'message': "Risk: " + "{:.2f}".format(model.predict(birth_year, averageDaysPastDue, totalLoanPayments, numberofInstallments, percentageofLateInstallment, interestRate) * 100) + "%"})
+
+
 
 @app.route('/insights', methods=['POST'])
 def insights():
     return render_template('insights.html')
 
 class Model:
+    def __init__(self, dirs):
+        self.models = [SingleModel(dir) for dir in dirs]
+
+    def predict(self, birth_year=None, avg_dpd=None, pay_count=None,
+                num_early_inst=None, perc_late_inst=None,
+                interest=None):
+        out = 0
+        for model in self.models:
+            out += model.predict(birth_year=birth_year, avg_dpd=avg_dpd, pay_count=pay_count,
+                                 num_early_inst=num_early_inst, perc_late_inst=perc_late_inst,
+                                 interest=interest)
+        out /= len(self.models)
+        return out
+
+
+class SingleModel:
     def __init__(self, dir="lgb"):
         dir_files = [f"{dir}/{file}" for file in os.listdir(dir)]
+        imp_path = f"{dir}/imp.pkl"
+        self.imp = None
+        if imp_path in dir_files:
+            self.imp = joblib.load(imp_path)
+        self.scaler = joblib.load(f"{dir}/scaler.pkl")
         self.models = []
         for file in dir_files:
-            if file.endswith(".pkl"):
+            if file.endswith("model.pkl"):
                 self.models.append(joblib.load(file))
         self.input = pd.read_parquet(dir + "/example.parquet")
         self.input.loc[:] = np.nan
@@ -67,31 +92,38 @@ class Model:
     # pay_count: [0, 60]
     # num_early_inst: [0, 60]
     # perc_late_inst: [0, 1]
-    # interest: [0, 1]
+    # interest: [0, 0.40]
     def predict(self, birth_year=None, avg_dpd=None, pay_count=None,
                 num_early_inst=None, perc_late_inst=None,
                 interest=None):
+        curr_input = deepcopy(self.input)
+        if self.imp is not None:
+            curr_input[:] = self.imp.transform(curr_input)
         if birth_year is not None:
             birth_date = (datetime(birth_year, 1, 1) - datetime(1900, 1, 1)).days
-            for column in self.input.columns:
+            for column in curr_input.columns:
                 if column.startswith("birthdate") or column.startswith("dateofbirth"):
-                    self.input.loc[0, column] = birth_date
+                    curr_input.loc[0, column] = birth_date
         if avg_dpd is not None:
-            self.input.loc[0, "avgdpdtolclosure24_3658938P"] = avg_dpd
+            curr_input.loc[0, "avgdpdtolclosure24_3658938P"] = avg_dpd
         if pay_count is not None:
-            self.input.loc[0, "pmtnum_254L"] = pay_count
+            curr_input.loc[0, "pmtnum_254L"] = pay_count
         if num_early_inst is not None:
-            self.input.loc[0, "numinstpaidearly3d_3546850L"] = num_early_inst
+            curr_input.loc[0, "numinstpaidearly3d_3546850L"] = num_early_inst
         if perc_late_inst is not None:
-            self.input.loc[0, "pctinstlsallpaidlate1d_3546856L"] = perc_late_inst
+            curr_input.loc[0, "pctinstlsallpaidlate1d_3546856L"] = perc_late_inst
         if interest is not None:
-            self.input.loc[0, "eir_270L"] = interest
+            curr_input.loc[0, "eir_270L"] = interest
+        curr_input[:] = self.scaler.transform(curr_input)
         out = 0
         for model in self.models:
-            out += float(model.predict_proba(self.input)[0, 1])
+            # print(model.coef_[0])
+            out += float(model.predict_proba(curr_input)[0, 1])
         out /= len(self.models)
-        self.input.loc[:] = np.nan
         return out
+
+
+
 
 
 
--- static/script.js
+++ static/script.js
@@ -8,55 +8,63 @@ function scrollToMain() {
     }
 }
 
-function rangeSlide(value) {
-    var sliderValues = {};
-    var sliders = document.querySelectorAll('.range');
-    var sliderValueSpans = document.querySelectorAll('.slider-value');
+function rangeSlide(selectedModels) {
+    return function(value) {
+        var sliderValues = {};
+        var sliders = document.querySelectorAll('.range');
+        var sliderValueSpans = document.querySelectorAll('.slider-value');
 
-    sliders.forEach(function(slider,index) {
-        sliderValues[slider.getAttribute('name')] = slider.value;
-        sliderValueSpans[index].innerHTML = slider.value;
-    });
+        sliders.forEach(function(slider, index) {
+            sliderValues[slider.getAttribute('name')] = slider.value;
+            sliderValueSpans[index].innerHTML = slider.value;
+        });
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/prediction", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var response = JSON.parse(xhr.responseText);
-                document.querySelector('.blob-text').textContent = response.message;
-            } else {
-                console.error('Error occurred while making AJAX request:', xhr.status);
-            }
-        }
-    };
-    xhr.send(JSON.stringify(sliderValues));
-}
-$(document).ready(function() {
-    $('#viewInsightsButton').on('click', function(event) {
-        event.preventDefault(); 
-        
+        var requestData = {sliderValues: sliderValues, selectedModels: selectedModels};
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/insights", true);
+        xhr.open("POST", "/prediction", true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.onreadystatechange = function() {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.status === 200) {
                     var response = JSON.parse(xhr.responseText);
-                    if (response && response.redirect) {
-                        console.log("Redirecting...");
-                        window.location.href = response.redirect;
-                    } else {
-                        console.error('Error: No redirect URL provided in response');
-                    }
+                    document.querySelector('.blob-text').textContent = response.message;
                 } else {
                     console.error('Error occurred while making AJAX request:', xhr.status);
                 }
             }
         };
-        xhr.send();
+        xhr.send(JSON.stringify(requestData));
+    }
+}
+
+$(document).ready(function() {
+    var selectedModels = []; 
+    $('#model-options2').addClass('clicked');
+    $('.model-options').on('click', function() {
+        var anyClicked = $('.model-options.clicked').length > 1 || !$(this).hasClass('clicked');
+        if (!anyClicked) {
+            return;
+        }
+        $(this).toggleClass('clicked');
+        
+        selectedModels = [];
+        $('.model-options.clicked').each(function() {
+            var modelName = $(this).text();
+            if (modelName === "Logistic") {
+                selectedModels.push("log");
+            } else if (modelName === "LightGBM") {
+                selectedModels.push("lgb");
+            } else if (modelName === "xgbBooster") {
+                selectedModels.push("xgb");
+            } else {
+                selectedModels.push(modelName);
+            }
+        });
+
+        rangeSlide(selectedModels)();
+    });
+
+    $('.range').on('change', function() {
+        rangeSlide(selectedModels)();
     });
 });
-
-
