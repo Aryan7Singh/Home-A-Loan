4b9d4b853412f2e3016af344d510ca23c2bb367d
Aryan Singh <aryan0.singh05@gmail.com>
2024-02-11 00:35:15 -0500

    all sliders

--- static/script.js
+++ static/script.js
@@ -8,10 +8,10 @@ function scrollToMain() {
     }
 }
 
-function rangeSlide(value) {
+function rangeSlide(featureName, value) {
     var sliderValues = {};
     var sliders = document.querySelectorAll('.range');
-    document.getElementById('rangeValue').innerHTML = value;
+    document.getElementById(featureName + 'Display').innerHTML = value;
     sliders.forEach(function(slider) {
         sliderValues[slider.getAttribute('name')] = slider.value;
     });
@@ -20,8 +20,8 @@ function rangeSlide(value) {
     xhr.open("POST", "/predict", true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify(sliderValues));
-    
 }
+
 document.getElementById('submit-button').addEventListener('click', function() {
     var sliderValues = {};
     var sliders = document.querySelectorAll('.range');
